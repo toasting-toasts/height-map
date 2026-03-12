25dc9610e2d1a80e504da758205af95f182de523
Toast <tostertonyacount@gmail.com>
2026-03-12 11:36:33 +0200

    Refactor random initialization and smoothing methods

--- heightmap.cpp
+++ heightmap.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <cmath>
 #include <ctime>
-#include <cstdlib>
 #include <random>
 #include <algorithm>
 
@@ -15,22 +14,17 @@ private:
     float current_from = 0;
     float current_to = 1;
 
-    void bind_random(){
-        static bool seeded = false;
-        if(!seeded){
-            srand(time(NULL));
-            seeded = true;
-        }
-    }
+    std::random_device rd;
+    std::mt19997 engine(rd());
+    std::uniform_real_distribution<float> dist(0.f, 1.f);
 
     void init_random(){
         for(int i=0; i<size*size; i++)
-        content[i] = rand()/(float)RAND_MAX;
+        content[i] = dist(engine);
     }
 
-    void init_tozero(){
-        for(int i=0; i<size*size; i++)
-        content[i] = 0;
+    void init_toval(float val){
+        std::fill(content.begin(), content.end(), val);
     }
 
     void init_random_recurse(
@@ -38,7 +32,7 @@ private:
         int x, int y, int region_size,
         int depth, float strenght
     ){
-        float layer = rand()/(float)RAND_MAX * strenght;
+        float layer = (dist(engine)*2-1) * strenght;
         for(int i = y; i < y + region_size; i++)
         for(int j = x; j < x + region_size; j++){
             vector_2d[i*size+j] = std::clamp(vector_2d[i*size+j]+layer, 0, 1);
@@ -57,23 +51,28 @@ private:
 
     void smooth_interpolation(int depth){
         float influence = 1.0f/(3*(1<<depth)-2);
-        std::vector<float> smoothed(size*size);
+        std::vector<float> smoothed(size*size, 0);
+        std::vector<int> weights(depth*2+1);
+
+        for(int i=-depth; i<=depth; i++){
+            weights[i+depth]= (1 << ( depth-std::abs(k) ));
+        }
         
         for (int i = 0; i<size; i++)
         for (int j = 0; j<size; j++)
         for (int k=-depth; k<=depth; k++){
              int idx = std::clamp(i+k, 0, size-1);
-             smoothed[i*size+j]+=(1<<(depth-abs(k)))*influence*content[idx*size+j];
+             smoothed[i*size+j]+=weights[k+depth]*influence*content[idx*size+j];
         }
 
         std::swap(smoothed, content);
-        std::fill(smoothed.begin(), smoothed.end(), 0);
+        std::fill(smoothed.begin(), smoothed.end(), 0.f);
         
         for (int i = 0; i<size; i++)
         for (int j = 0; j<size; j++)
         for (int k=-depth; k<=depth; k++){
              int idx = std::clamp(j+k, 0, size-1);
-             smoothed[i*size+j]+=(1<<(depth-abs(k)))*influence*content[i*size+idx];
+             smoothed[i*size+j]+= weights[k+depth]*influence*content[i*size+idx];
         }
         
         std::swap(smoothed, content);
@@ -89,8 +88,7 @@ private:
             content[i] += val;
     }
 public:
-    Heightmap(int size) : size(size) {
-        bind_random();
+    Heightmap(int size) : size(size){
         content.resize(size*size);
         if ((size&(size-1)) != 0) throw "Size must be a power of two for some functions to work!";
     };
@@ -113,13 +111,13 @@ public:
     }
     
     void strong_random_init(int depth, float strenght=0.5){
-      init_tozero();
+      init_toval(0);
       init_random_recurse(content, 0, 0, size, depth, strenght);
     }
     void random_init(){
      init_random();
     }
-    std::vector<float>& get_1d_vector(){
+    const std::vector<float>& get_1d_vector() const {
      return content;
     }
     void interpolate_V1(float coef, int repeats){
